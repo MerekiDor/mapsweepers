8dfca767281868ccb5fc7d17cb86dddd21d4affe
JonahSoldier <86782614+JonahSoldier@users.noreply.github.com>
2026-08-03 19:19:44 +0100

    Workshop content download for ZMod

--- gamemodes/mapsweepers/gamemode/init.lua
+++ gamemodes/mapsweepers/gamemode/init.lua
@@ -54,6 +54,8 @@ include "_main/server/sv_turrets.lua"
 include "_main/server/sv_damagehandling.lua"
 include "_main/server/sv_rangecap.lua"
 
+resource.AddWorkshop("3564041088") --TODO: TEMPORARY. Needed for ZMod to work.
+
 -- // Mission Includes {{{
 	do 
 		include "missions/sv_missions.lua"
